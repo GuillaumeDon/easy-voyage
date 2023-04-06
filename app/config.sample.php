7e499c8a9e7218189fa46043917970fcfef29e5c
Guillaume Donizetti <glm.donizetti@gmail.com>
2023-04-06 14:07:23 +0200

    ajout du config.sample et du readMe

---
+++ app/config.sample.php
@@ -0,0 +1,13 @@
+<?php 
+const DB_HOST = '';
+const DB_NAME = '';
+const DB_USER = '';
+const DB_PASS = '';
+const PATH_ROOT = "http://localhost/easy-voyage";
+const PATH_ROOT2 = "/easy-voyage/";
+const PATH_PUBLIC = "public";
+const PATH_TPL = "easy-voyage/views";
+const PATH_CSS = "css";
+const PATH_JS = "js";
+const PATH_IMG = "img";
+
--- biblio/bdd/02-users.sql
+++ biblio/bdd/02-users.sql
@@ -3,6 +3,11 @@ GRANT ALL PRIVILEGES ON bdd_easy_voyage.* TO 'superadmin'@'localhost';
 FLUSH PRIVILEGES;
 
 
--- private $username = "superadmin";
---     private $password = "3m35353dpSe3curi_53353seLvlElite40675";
---     private $conn;
+
+
+	CREATE USER ‘admin’@'localhost' IDENTIFIED BY ‘zrbzbfnnezvz’;
+GRANT
+ SELECT, INSERT, UPDATE, DELETE, EXEC 
+ON bdd_easy_voyage.* 
+TO ‘admin’@'localhost';
+FLUSH PRIVILEGES;
